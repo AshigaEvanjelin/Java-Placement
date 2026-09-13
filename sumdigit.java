95add7393dd4eaf8d8ce34ba6a7e9995cc27ac6f
Ashiga35 <ashijeba06@gmail.com>
2026-09-13 11:23:47 +0530

    finding sum of digits

---
+++ sumdigit.java
@@ -0,0 +1,16 @@
+public class sumdigit {
+    public static void main(String[] args) {
+        int num = 56748;
+        int sum = 0;
+
+        while(num !=0){
+            int digit = num % 10;
+            sum = sum + digit;
+            num = num / 10;
+        }
+        
+        System.out.println(sum);
+    }
+    
+    
+}
