00b00b3e1f4bd2c65826d255c4f26dcb840d58e4
Ashiga35 <ashijeba06@gmail.com>
2026-09-19 02:17:02 +0530

    Finding maximum in the array

---
+++ Array/maxarray.java
@@ -0,0 +1,36 @@
+package Array;
+
+import java.util.Scanner;
+
+public class maxarray {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[3][3];
+        
+
+        for(int i = 0; i < 3 ; i++){
+            for(int  j = 0 ; j < 3; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int max = arr[0][0];
+
+        
+
+        for(int i = 0; i < 3 ; i++){
+            for(int  j = 0 ; j < 3; j++){
+                if(arr[i][j] > max){
+                    max = arr[i][j];
+
+                }
+                
+            }
+            
+        }
+        System.out.print(max);
+
+    }
+    
+}
